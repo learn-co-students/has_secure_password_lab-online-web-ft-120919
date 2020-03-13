a731f5ed8252b6f9b1eb151813a3b3091c7745a1
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-12 17:05:38 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'users#new'
   post '/', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get '/new', to: 'sessions#new'
+  post '/new', to: 'sessions#create'
   get '/user', to: 'users#show'
 end
