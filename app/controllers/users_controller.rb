9a4d9f2e0eecbf44045f9a88ab51b1e9723e4615
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-05-08 18:40:57 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def welcome
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to new_user_path unless @user.password == @user.password_confirmation
+    session[:user_id] = @user.id
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
